eb3951edfb70bb5fe3ae708913561973c9ae111e
SergeyB19 <my.h.m@yandex.ru>
2024-02-14 21:05:38 +0300

    itproger(Create model)

---
+++ src/main/java/com/itproger/blog/models/Post.java
@@ -0,0 +1,52 @@
+package com.itproger.blog.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title, anonse, full_text;
+    private int views;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAnonse() {
+        return anonse;
+    }
+
+    public void setAnonse(String anonse) {
+        this.anonse = anonse;
+    }
+
+    public String getFull_text() {
+        return full_text;
+    }
+
+    public void setFull_text(String full_text) {
+        this.full_text = full_text;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+}
